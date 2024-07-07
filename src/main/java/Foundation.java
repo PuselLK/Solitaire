6816cba491a9d25b56386746b1bc1432362adc7e
1krellen <lukas.krellenberg@studium.uni-hamburg.de>
2024-07-07 20:31:03 +0200

    Updated JavaDoc of Foundation.java

--- src/main/java/Foundation.java
+++ src/main/java/Foundation.java
@@ -1,8 +1,15 @@
 import java.util.Stack;
 
+/**
+ * Represents a foundation in a game of solitaire. A foundation is a stack of cards, where each card
+ * must be of the same suit and one value higher than the previous card.
+ */
 public class Foundation {
     private final Stack<Card> _foundation;
 
+    /**
+     * Creates a new foundation with an empty stack of cards
+     */
     public Foundation() {
         _foundation = new Stack<>();
     }
@@ -11,7 +18,7 @@ public class Foundation {
      * Places a given card on the foundation Stack if the move is valid
      *
      * @param card The card to be placed
-     * @return True if the card has been placed on the foundation, false otherwise
+     * @return True if the card has been placed on the foundation and therefore the move was valid, false otherwise
      */
     public boolean placeCard(Card card) {
         if (isValidMove(card)) {
@@ -23,7 +30,9 @@ public class Foundation {
 
 
     /**
-     * Gets the top card from the foundation, removes and returns it.
+     * Removes the top card from the foundation
+     *
+     * @throws RuntimeException if the foundation is empty
      */
     public void pickUpCard() {
         if (_foundation.isEmpty()) {
@@ -34,6 +43,8 @@ public class Foundation {
 
     /**
      * Checks if a card can be placed on the foundation
+     * A card can be placed on the foundation if it is the same suit as the top card of the foundation
+     * and its value is one higher
      *
      * @param card The card to be placed
      * @return True if the card can be placed on the foundation, false otherwise
@@ -54,6 +65,11 @@ public class Foundation {
         _foundation.clear();
     }
 
+    /**
+     * Returns the top card of the foundation without removing it
+     *
+     * @return The top card of the foundation, or null if the foundation is empty
+     */
     public Card peekFoundation() {
         if (_foundation.isEmpty()) {
             return null;
@@ -61,6 +77,11 @@ public class Foundation {
         return _foundation.peek();
     }
 
+    /**
+     * Checks if the foundation is empty
+     *
+     * @return True if the foundation is empty, false otherwise
+     */
     public boolean isEmpty() {
         return _foundation.isEmpty();
     }
