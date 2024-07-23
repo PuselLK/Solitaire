95c7551c3f1d554735fa15dd5489fe90f383f87d
1krellen <lukas.krellenberg@studium.uni-hamburg.de>
2024-07-24 00:22:50 +0200

    Added First Test to SolitaireTest.java

--- src/test/java/Game/SolitaireTest.java
+++ src/test/java/Game/SolitaireTest.java
@@ -1,50 +1,35 @@
 package Game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class SolitaireTest {
     private Solitaire solitaire;
-    private Solitaire solitaireMock;
-    private Card mockCard;
-    private Deck mockDeck;
 
-//    @BeforeEach
-//    void setUp() {
-//        solitaire = new Solitaire();
-//        solitaireMock = mock(Solitaire.class);
-//        mockCard = mock(Card.class);
-//        mockDeck = mock(Deck.class);
-//    }
+    @BeforeEach
+    void setUp() {
+        solitaire = new Solitaire();
+    }
 
-//    @Test
-//    void constructor_shouldPlaceCorrectNumberOfCardsOnTableau() {
-//        int totalCards = 0;
-//        int tableauSize = 1;
-//        for (Tableau tableau : solitaire.get_tableausArray()) {
-//            assertEquals(tableauSize, tableau.getTableauSize());
-//            tableauSize++;
-//            totalCards += tableau.getTableauSize();
-//        }
-//        assertEquals(28, totalCards);
-//    }
-//
-//    @Test
-//    void placeCardOnClick_shouldReturnTrue_WhenCardIsPlacedOnFoundation_And_CardIsAce() {
-//        Card card = new Card(Card.Suit.club, 1);
-//        assertTrue(solitaire.placeCardOnClick(card));
-//        assertTrue(solitaire.get_foundationsArray()[0].peekFoundation().equals(card));
-//    }
+    @Test
+    void SolitaireConstructor_ShouldSetSolitaireCorrectly() {
+        for (Foundation foundation : solitaire.get_foundationsArray()) {
+            assertTrue(foundation.isEmpty());
+        }
+
+        for (Tableau tableau : solitaire.get_tableausArray()) {
+            int tableauSize = 0;
+            tableauSize++;
+            assertEquals(tableauSize, tableau.getTableauSize());
+            assertTrue(tableau.peekTableau().isVisible());
+        }
+
+        assertTrue(solitaire.get_deck().isDiscardPileEmpty());
+        assertEquals(24, solitaire.get_deck().get_deck().size());
+    }
 
 
-//    @Test
-//    void isGameFinished_ShouldReturnTrue_WhenAllFoundationsHaveKings() {
-//        // Mock foundations to simulate a finished game
-//        Card.Suit[] suits = {Card.Suit.club, Card.Suit.spade, Card.Suit.diamond, Card.Suit.heart};
-//
-//        for (int i = 0; i < suits.length; i++) {
-//            Foundation foundation = solitaire.get_foundationsArray()[i];
-//            for (int rank = 1; rank <= 13; rank++) {
-//                foundation.placeCard(new Card(suits[i], rank));
-//            }
-//        }
-//        assertTrue(solitaire.isGameFinished());
-//    }
 }
